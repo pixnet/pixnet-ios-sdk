a640c63edc78f06e91b671db17cbf3296e0b600f
dolphin su <dolphinsu@pixnet.tw>
2014-03-14 17:43:45 +0800

    remove usless importer.

--- PIXNET-iOS-SDK/PIXNETSDK.h
+++ PIXNET-iOS-SDK/PIXNETSDK.h
@@ -29,19 +29,6 @@
  *  @return 該部落格的基本資訊
  */
 - (void)getBlogInformationWithUserID:(NSString *)userID completion:(RequestCompletion)completion;
-/**
- *  讀取使用者部落格分類資訊
- *
- *  @param userID 痞客邦帳號
- *  @param passwd 如果指定使用者的 Blog 被密碼保護，則需要指定這個參數以通過授權
- *
- *  @return 部落格分類資訊, 密碼錯誤會回傳 status : password Error
- */
-//- (NSDictionary *)PIXgetBlogCategories:(NSString *)userID withPassword:(NSString *)passwd;
-
-
-
-//- (NSDictionary *)PIXpostBlogCategories:(NSString *)cateName withType:(PIXCategoriesType *)type;
 
 
 @end
