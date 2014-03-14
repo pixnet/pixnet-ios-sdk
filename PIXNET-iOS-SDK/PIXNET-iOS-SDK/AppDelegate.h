a62e70cdbaa88846bcb7e6193d1823dc7cd9bb33
dolphin su <dolphinsu@pixnet.tw>
2014-03-14 16:29:51 +0800

    projects created.

---
+++ PIXNET-iOS-SDK/PIXNET-iOS-SDK/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PIXNET-iOS-SDK
+//
+//  Created by Dolphin Su on 3/14/14.
+//  Copyright (c) 2014 Dolphin Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PIXNET-iOS-SDK/PIXNET-iOS-SDK/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PIXNET-iOS-SDK
+//
+//  Created by Dolphin Su on 3/14/14.
+//  Copyright (c) 2014 Dolphin Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
