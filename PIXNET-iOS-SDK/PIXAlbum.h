86498210bf4844cb41ffc344ba72334ed0ab1505
dolphin su <dolphinsu@pixnet.tw>
2014-03-20 11:16:27 +0800

    fetch album sets method added.

--- PIXNET-iOS-SDK/PIXAlbum.h
+++ PIXNET-iOS-SDK/PIXAlbum.h
@@ -17,16 +17,27 @@
 /**
  *  列出相簿列表 http://developer.pixnet.pro/#!/doc/pixnetApi/albumSetfolders
  *
- *  @param userName 指定要回傳的使用者資訊,必填欄位
- *  @param trimUser 是否每篇文章都要回傳作者資訊, 如果設定為 1, 則就不回傳. 預設是 0
- *  @param page     頁數, 預設為1
- *  @param perPage  每頁幾筆, 預設為100
+ *  @param userName   指定要回傳的使用者資訊,必要參數
+ *  @param trimUser   是否每篇文章都要回傳作者資訊, 如果設定為 YES, 則就不回傳. 預設是 NO
+ *  @param page       頁數, 預設為1
+ *  @param perPage    每頁幾筆, 預設為100
  *  @param completion RequestCompletion
  */
 -(void)fetchAlbumListWithUserName:(NSString *)userName trimUser:(BOOL)trimUser page:(NSInteger)page perPage:(NSInteger)perPage completion:(RequestCompletion)completion;
 
 #pragma Sets
-
+/**
+ *  列出個人相簿個人 Sets http://developer.pixnet.pro/#!/doc/pixnetApi/albumSets
+ *
+ *  @param userName   指定要回傳的使用者資訊,必要參數
+ *  @param parentID   可以藉此指定拿到特定相簿資料夾底下的相簿
+ *  @param trimUser   是否每篇文章都要回傳作者資訊, 如果設定為 1, 則就不回傳. 預設是 NO
+ *  @param page       頁數, 預設為1
+ *  @param perPage    每頁幾筆, 預設為100
+ *  @param shouldAuth 請求對象是被保護時，這裡請給 YES
+ *  @param completion RequestCompletion
+ */
+-(void)fetchAlbumSetsWithUserName:(NSString *)userName parentID:(NSString *)parentID trimUser:(BOOL)trimUser page:(NSInteger)page perPage:(NSInteger)perPage shouldAuth:(BOOL)shouldAuth completion:(RequestCompletion)completion;
 #pragma Folders
 
 #pragma Element
--- PIXNET-iOS-SDK/PIXAlbum.m
+++ PIXNET-iOS-SDK/PIXAlbum.m
@@ -13,26 +13,55 @@
     if (userName == nil || userName.length == 0) {
         completion(NO, nil, @"userName 是必要參數");
     }
-    NSDictionary *params = @{@"user": userName,
-                             @"trim_user": @(trimUser),
-                             @"page": @(page),
-                             @"per_page": @(perPage)};
+    NSMutableDictionary *params = [NSMutableDictionary new];
+    params[@"user"] = userName;
+    params[@"trim_user"] = @(trimUser);
+    params[@"page"] = @(page);
+    params[@"per_page"] = @(perPage);
     [[PIXAPIHandler new] callAPI:@"album/setfolders" parameters:params requestCompletion:^(BOOL succeed, id result, NSString *errorMessage) {
         if (succeed) {
-            NSError *jsonError;
-            NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:result options:NSJSONReadingAllowFragments error:&jsonError];
-            if (jsonError == nil) {
-                if ([dict[@"error"] intValue] == 0) {
-                    completion(YES, dict, nil);
-                } else {
-                    completion(NO, nil, dict[@"message"]);
-                }
-            } else {
-                completion(NO, nil, jsonError.localizedDescription);
-            }
+            [self succeedHandleWithData:result completion:completion];
         } else {
             completion(NO, nil, errorMessage);
         }
     }];
+}
+
+-(void)fetchAlbumSetsWithUserName:(NSString *)userName parentID:(NSString *)parentID trimUser:(BOOL)trimUser page:(NSInteger)page perPage:(NSInteger)perPage shouldAuth:(BOOL)shouldAuth completion:(RequestCompletion)completion{
+    if (userName == nil || userName.length == 0) {
+        completion(NO, nil, @"userName 是必要參數");
+    }
+    NSMutableDictionary *params = [NSMutableDictionary new];
+    params[@"user"] = userName;
+    params[@"trim_user"] = @(trimUser);
+    params[@"page"] = @(page);
+    params[@"per_page"] = @(perPage);
+    if (parentID != nil && parentID.length > 0) {
+        params[@"parent_id"] = parentID;
+    }
+    if (shouldAuth) {
+        
+    } else {
+        [[PIXAPIHandler new] callAPI:@"album/sets" parameters:params requestCompletion:^(BOOL succeed, id result, NSString *errorMessage) {
+            if (succeed) {
+                [self succeedHandleWithData:result completion:completion];
+            } else {
+                completion(NO, nil, errorMessage);
+            }
+        }];
+    }
+}
+-(void)succeedHandleWithData:(id)data completion:(RequestCompletion)completion{
+    NSError *jsonError;
+    NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&jsonError];
+    if (jsonError == nil) {
+        if ([dict[@"error"] intValue] == 0) {
+            completion(YES, dict, nil);
+        } else {
+            completion(NO, nil, dict[@"message"]);
+        }
+    } else {
+        completion(NO, nil, jsonError.localizedDescription);
+    }
 }
 @end
