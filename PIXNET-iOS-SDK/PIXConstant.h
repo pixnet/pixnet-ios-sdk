b67d4367378295a85bae1c2301bddfd44c83dabd
jnlin <jnlin@pixnet.tw>
2014-03-22 17:11:26 +0800

    Add Constants headers

---
+++ PIXNET-iOS-SDK/PIXConstant.h
@@ -0,0 +1,15 @@
+//
+//  PIXConstant.h
+//  PIXNET-iOS-SDK
+//
+//  Created by jnlin on 2014/3/22.
+//  Copyright (c) 2014年 Dolphin Su. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PIXConstant : NSObject
+
+FOUNDATION_EXPORT NSString *const MsgFmtRequiredParameter;
+
+@end
