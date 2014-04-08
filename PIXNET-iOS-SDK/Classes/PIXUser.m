f0e2eed594fc88389799f9fb12b76b4322bbb8b8
jnlin <jnlin@pixnet.tw>
2014-04-08 17:26:56 +0800

    Implement getUserWithUserName

--- PIXNET-iOS-SDK/Classes/PIXUser.m
+++ PIXNET-iOS-SDK/Classes/PIXUser.m
@@ -10,14 +10,36 @@
 
 @implementation PIXUser
 
+-(void)succeedHandleWithData:(id)data completion:(PIXHandlerCompletion)completion{
+    NSError *jsonError;
+    NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&jsonError];
+    if (jsonError == nil) {
+        if ([dict[@"error"] intValue] == 0) {
+            completion(YES, dict, nil);
+        } else {
+            completion(NO, nil, dict[@"message"]);
+        }
+    } else {
+        completion(NO, nil, jsonError.localizedDescription);
+    }
+}
+
+
 -(void)getUserWithUserName:(NSString *)userName completion:(PIXHandlerCompletion)completion{
     if (userName == nil || userName.length == 0) {
         completion(NO, nil, @"Missing userName");
     }
-    NSString *api = [NSString stringWithFormat:@"users/%@", userName];
     NSMutableDictionary *params = [NSMutableDictionary new];
     
-    
+    [[PIXAPIHandler new] callAPI:[NSString stringWithFormat:@"users/%@", userName] parameters:params requestCompletion:^(BOOL succeed, id result, NSString *errorMessage) {
+        //檢查出去的參數
+        if (succeed) {
+            [self succeedHandleWithData:result completion:completion];
+        } else {
+            completion(NO, nil, errorMessage);
+        }
+
+    }];
 }
 
 
