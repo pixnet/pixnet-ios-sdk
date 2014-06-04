24f65b8637ee7b95a5f603ee7e6c005549f2c902
dolphin su <dolphinsu@pixnet.tw>
2014-06-04 14:09:22 +0800

    Index class added.

---
+++ PIXNET-iOS-SDK/Classes/PIXIndex.h
@@ -0,0 +1,13 @@
+//
+//  PIXIndex.h
+//  PIXNET-iOS-SDK
+//
+//  Created by Dolphin Su on 6/4/14.
+//  Copyright (c) 2014 Dolphin Su. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PIXIndex : NSObject
+
+@end
