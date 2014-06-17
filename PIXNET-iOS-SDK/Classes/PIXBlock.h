5240f9d1fd0e30b35a531ab7c3c7e74404699a56
dolphinSuPixnet <dolphinsu@pixnet.tw>
2014-06-17 11:48:31 +0800

    PIXBlock class added.

---
+++ PIXNET-iOS-SDK/Classes/PIXBlock.h
@@ -0,0 +1,14 @@
+//
+//  PIXBlock.h
+//  PIXNET-iOS-SDK
+//
+//  Created by Dolphin on 2014/6/17.
+//  Copyright (c) 2014年 Dolphin Su. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PIXAPIHandler.h"
+
+@interface PIXBlock : NSObject
+
+@end
---
+++ PIXNET-iOS-SDK/Classes/PIXBlock.m
@@ -0,0 +1,13 @@
+//
+//  PIXBlock.m
+//  PIXNET-iOS-SDK
+//
+//  Created by Dolphin on 2014/6/17.
+//  Copyright (c) 2014年 Dolphin Su. All rights reserved.
+//
+
+#import "PIXBlock.h"
+
+@implementation PIXBlock
+
+@end
