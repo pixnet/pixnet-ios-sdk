301d0da2111136e957738f9eb1a08f9aa32c678f
dolphinSuPixnet <dolphinsu@pixnet.tw>
2015-11-03 11:16:24 +0800

    fix error localization in testing

--- PIXNET-iOS-SDK/Classes/NSError+PIXCategory.m
+++ PIXNET-iOS-SDK/Classes/NSError+PIXCategory.m
@@ -21,9 +21,14 @@
 +(instancetype)PIXErrorWithServerResponse:(NSDictionary *)response{
     //這個 bundle 的取得法超機歪！
     NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"PIXNET-iOS-SDK" ofType:@"bundle"];
-    NSBundle *bundle = [NSBundle bundleWithPath:bundlePath];
+    NSString *message = nil;
+    if (bundlePath) {
+        NSBundle *bundle = [NSBundle bundleWithPath:bundlePath];
+        message = NSLocalizedStringFromTableInBundle(response[@"code"], @"Localizable", bundle, nil);
+    } else {
+        message = NSLocalizedStringFromTableInBundle(response[@"code"], @"Localizable", [NSBundle mainBundle], nil);
+    }
     //直接用 code 字串透過 Localizable.strings 轉成中文或英文說明
-    NSString *message = NSLocalizedStringFromTableInBundle(response[@"code"], @"Localizable", bundle, nil);
     NSInteger code = [response[@"code"] integerValue];
     NSError *error = [NSError errorWithDomain:kPIXErrorDomain code:code userInfo:@{NSLocalizedDescriptionKey: message}];
     return error;
