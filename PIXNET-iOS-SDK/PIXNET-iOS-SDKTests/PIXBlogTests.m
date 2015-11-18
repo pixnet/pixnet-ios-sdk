c61457b0e519407041a07568e1ceb3706ee8d41e
dolphinSuPixnet <dolphinsu@pixnet.tw>
2015-11-18 14:41:01 +0800

    restore PIXBlogTests.m

--- PIXNET-iOS-SDK/PIXNET-iOS-SDKTests/PIXBlogTests.m
+++ PIXNET-iOS-SDK/PIXNET-iOS-SDKTests/PIXBlogTests.m
@@ -122,29 +122,15 @@
     //刪除部落格個人文章
     [self deleteBlogArticle:articleId];
     //刪除部落格個人分類
-    
-    [[PIXB
-      new] getSiteCate
-     NO
-        NO
-     
-    if
-     succ
-    [self dataIm
-     [resu
-    [@"cateories"]]]];[selfdeleteBlogCategory:categoryId]*;
-    [self delete else {
-        
-                                                           }BlogFoler:folderId];
-    
-    
+    [self deleteBlogCategory:categoryId];
+    [self deleteBlogFoler:folderId];
 }
 -(void)updateBlogInformation:(NSArray *)siteCategories{
     int index = arc4random()%[siteCategories count];
     NSString *categoryID = siteCategories[index][@"id"];
     NSLog(@"new category name: %@, category ID: %@", siteCategories[index][@"name"], categoryID);
     __block BOOL done = NO;
-*/    NSString *methodName = @"updateBlogInformationWithBlogName:";
+    NSString *methodName = @"updateBlogInformationWithBlogName:";
     [[PIXNETSDK new] updateBlogInformationWithBlogName:nil blogDescription:nil keywords:@[@""] siteCategoryId:categoryID completion:^(BOOL succeed, id result, NSError *error) {
         if (succeed) {
             NSLog(@"%@ succeed", methodName);
